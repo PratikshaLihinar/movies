64498ebea8c944960e749a87661a4dafb94d9764
pratiksha <pratiksha.lihinar1@gmail.com>
2025-06-23 19:23:56 +0530

    clean code

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -5,14 +5,10 @@ import useFetch from '../hooks/useFetch';
 const MovieList = ({apiPath}) => {
   const {data: movies}= useFetch(apiPath);
   
-
-
-
   return (
     <main>
       <section className='max-w-7xl mx-auto py-7'>
         <div className='flex justify-start flex-wrap'>
-          {console.log(movies)}
           { movies.map((movie) => 
           <Card key={movie.id} movie={movie} />
           )}
